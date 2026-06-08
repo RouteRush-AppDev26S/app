c75b9e76707711ae436daad990e20b0e1aaaa68b
Hans Wornik <hwornik@gmail.com>
2026-06-08 21:32:23 +0200

    fehlerkotrektur

---
+++ app/src/main/java/com/example/appdevproject26s/DataModule.kt
@@ -0,0 +1,36 @@
+package com.example.appdevproject26s
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return context.dataStore
+    }
+
+    @Provides
+    @Singleton
+    fun provideMapSettingsRepository(dataStore: DataStore<Preferences>): MapSettingsRepository {
+        return MapSettingsRepository(dataStore)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFusedLocationClient(@ApplicationContext context: Context): FusedLocationProviderClient {
+        return LocationServices.getFusedLocationProviderClient(context)
+    }
+}
