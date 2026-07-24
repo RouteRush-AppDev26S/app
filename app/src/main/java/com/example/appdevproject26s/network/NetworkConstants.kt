9d8fea6a6652d9a4d9486dcb94302b72b015f473
TJCur <jackcurrie2310@gmail.com>
2026-07-24 22:08:29 +0200

    extracts BackendBaseURL into NetworkConstants.kt

---
+++ app/src/main/java/com/example/appdevproject26s/network/NetworkConstants.kt
@@ -0,0 +1,6 @@
+package com.example.appdevproject26s.network
+
+const val BACKEND_BASE_URL = "https://backend-g7be.onrender.com/"
+const val BACKEND_REST_URL = "https://$BACKEND_BASE_URL"
+
+const val BACKEND_WS_URL = "ws://$BACKEND_BASE_URL/ws"
