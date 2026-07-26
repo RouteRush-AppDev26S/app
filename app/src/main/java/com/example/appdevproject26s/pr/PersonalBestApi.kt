4c3316faf8fd8b386287a6710ea186c72a70817e
Moritz2k1 <moritz.gutschi@outlook.com>
2026-07-26 11:13:44 +0200

    Implemented the PR api + extended BackendNetworkModule.kt again

---
+++ app/src/main/java/com/example/appdevproject26s/pr/PersonalBestApi.kt
@@ -0,0 +1,15 @@
+package com.example.appdevproject26s.pr
+
+import retrofit2.http.GET
+
+data class PersonalBest(
+    val type: String,
+    val description: String,
+    val value: Double,
+    val achievedAt: String
+)
+
+interface PersonalBestApi {
+    @GET("personal-bests")
+    suspend fun getPersonalBests(): List<PersonalBest>
+}
